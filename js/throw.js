24da5532ec62d6f57e494b8a7c7d1f377ec4e067
Pankaj Agarwal <pankaj3377@gmail.com>
2022-07-22 23:32:11 +0530

    Collide

--- js/throw.js
+++ js/throw.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent("ball", {
+AFRAME.registerComponent("ball-element", {
 	init: function () {
 		this.throwBall();
 	},
@@ -6,10 +6,11 @@ AFRAME.registerComponent("ball", {
 	throwBall: function () {
 		document.addEventListener("keydown", (e) => {
 			if (e.key === "z") {
-				var ball = document.querySelector("#ball");
+				var ball = document.createElement("a-entity");
+
 				ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf");
 
-				ball.setAttribute("scale", { x: 4, y: 4, z: 4 });
+				ball.setAttribute("scale", { x: 6, y: 6, z: 6 });
 
 				var camera = document.querySelector("#camera").object3D;
 
@@ -26,7 +27,39 @@ AFRAME.registerComponent("ball", {
 					z: campos.z - 0.2,
 				});
 				console.log(campos);
+
+				var scene = document.querySelector("#scene");
+
+				//set the bullet as the dynamic entity
+				ball.setAttribute("dynamic-body", {
+					shape: "sphere",
+					mass: "20",
+				});
+
+				//add the collide event listener to the bullet
+				ball.addEventListener("collide", this.collide);
+				console.log(ball);
+				scene.appendChild(ball);
 			}
 		});
+	},
+
+	collide: function (e) {
+		console.log("f");
+		var element = e.target.body.el;
+		var elementhit = e.detail.body.el;
+		console.log(elementhit.id);
+		if (elementhit.id.includes("pin")) {
+			var impulse = new CANNON.Vec3(0, 0.65, -35);
+			var worldPoint = new CANNON.Vec3().copy(
+				elementhit.getAttribute("position")
+			);
+
+			elementhit.body.applyForce(impulse, worldPoint);
+
+			element.removeEventListener("collide", this.collide);
+			var scene = document.querySelector("#scene");
+			scene.removeChild(element);
+		}
 	},
 });
